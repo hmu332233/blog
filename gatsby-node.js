112bcd4585d11ffd8502821831be8703a21545ff
minung.han <hmu332233@gmail.com>
2020-06-07 06:45:28 +0900

    feat: category 페이지 추가

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,5 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions;
-  const blogPostTemplate = require.resolve(`./src/templates/Post.jsx`);
   const result = await graphql(`
     {
       allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }, limit: 1000) {
@@ -19,20 +11,39 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
           }
         }
       }
+
+      categories: allMarkdownRemark {
+        group(field: frontmatter___category) {
+          category: fieldValue
+        }
+      }
     }
   `);
+
   // Handle errors
   if (result.errors) {
     reporter.panicOnBuild(`Error while running GraphQL query.`);
     return;
   }
+
+  // post page
   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
     createPage({
       path: node.frontmatter.slug,
-      component: blogPostTemplate,
+      component: require.resolve(`./src/templates/Post.jsx`),
       context: {
-        // additional data can be passed via context
         slug: node.frontmatter.slug,
+      },
+    });
+  });
+
+  // category page
+  result.data.categories.group.forEach(({ category }) => {
+    createPage({
+      path: `/category/${category}`,
+      component: require.resolve(`./src/templates/PostList.jsx`),
+      context: {
+        category,
       },
     });
   });
---
+++ src/templates/PostList.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+import Layout from '@components/Layout';
+import SEO from '@components/Seo';
+import PostLink from '@components/PostLink';
+import CategoryList from '@containers/CategoryList';
+
+const PostList = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => {
+  const Posts = edges
+    .filter((edge) => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+    .map((edge) => <PostLink key={edge.node.id} post={edge.node} />);
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <CategoryList />
+      {Posts}
+    </Layout>
+  );
+};
+
+export default PostList;
+
+export const query = graphql`
+  query($category: String!) {
+    allMarkdownRemark(filter: { frontmatter: { category: { eq: $category } } }, sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(format: PLAIN, pruneLength: 250)
+          frontmatter {
+            category
+            date(formatString: "MMMM DD, YYYY")
+            slug
+            title
+          }
+        }
+      }
+    }
+  }
+`;
