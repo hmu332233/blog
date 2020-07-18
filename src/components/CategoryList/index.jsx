9088f598635fb6732996f359f70e68f070701496
minung.han <hmu332233@gmail.com>
2020-07-18 12:00:11 +0900

    feat: ui 간격 일부 조정

--- src/components/CategoryList/index.jsx
+++ src/components/CategoryList/index.jsx
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 
 import { Link } from 'gatsby';
 
+import classNames from 'classnames';
+
 const Category = (props) => <Link to={props.link} className="ml-2 first:ml-0 inline-block py-1 px-3 rounded bg-blue-100 text-blue-500 text-sm font-medium tracking-widest" activeClassName="border border-blue-500 bg-transparent">{`${props.text}`}</Link>;
 
 function CategoryList(props) {
   return (
-    <div className="flex">
+    <div className={classNames('flex', props.className)}>
       {props.categories.map(({ text, link }) => (
         <Category link={link} text={text} />
       ))}
@@ -16,6 +18,7 @@ function CategoryList(props) {
 }
 
 CategoryList.propTypes = {
+  className: PropTypes.string,
   categories: PropTypes.array,
 };
 CategoryList.defaultProps = {
