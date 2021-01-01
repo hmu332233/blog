ed9f0171f8dfd58be3bd08b71ec6a9b40dfbfe29
minung.han <hmu332233@gmail.com>
2021-01-02 00:14:49 +0900

    feat: config 일부 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,8 +60,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `minung--dev-log`,
+        short_name: `blog`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
