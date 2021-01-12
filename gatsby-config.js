8512999f2a17ed1beffd887e34a7097e9d1f3a95
minung.han <hmu332233@gmail.com>
2021-01-12 15:49:31 +0900

    feat: google analytics 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,21 @@ module.exports = {
     }
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "G-QFKWVWVQEC",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
