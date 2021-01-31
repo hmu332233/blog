feb92a6e6086b2edc74063f3b753ae214a559451
minung.han <hmu332233@gmail.com>
2021-01-31 20:47:21 +0900

    fix: GA V4 대응 플러그인 변경

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,18 +14,12 @@ module.exports = {
   plugins: [
     `gatsby-plugin-sitemap`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "G-7J4V8XW5JH",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-7J4V8XW5JH", // Google Analytics / GA
+        ],
       },
     },
     `gatsby-plugin-react-helmet`,
