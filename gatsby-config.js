2e98dd87c04719e08e200194a4f8c62c26869f75
minung.han <hmu332233@gmail.com>
2021-06-03 02:39:46 +0900

    feat: 마크다운 코드 하이라이팅 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,6 +41,7 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          `gatsby-remark-prismjs`,
           {
             resolve: `gatsby-remark-images`,
             options: {
