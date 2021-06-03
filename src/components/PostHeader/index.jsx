a823ad2bf70f007ea60ec811b5c14726ddf49f04
minung.han <hmu332233@gmail.com>
2021-06-04 01:23:04 +0900

    feat: Category에 div type 추가

--- src/components/PostHeader/index.jsx
+++ src/components/PostHeader/index.jsx
@@ -6,7 +6,7 @@ import Category from '@components/Category';
 function PostHeader(props) {
   return (
     <div className="clearfix mb-10">
-      <Category text={props.category} link={`/category/${props.category}`} />
+      <Category text={props.category} link={`/category/${props.category}`} active />
       <h2 className="sm:text-3xl text-2xl title-font font-black text-gray-900 mt-4 mb-4">{props.title}</h2>
       <span className="mt-1 text-gray-500 text-sm float-right">{props.date}</span>
     </div>
--- src/components/PostLink/index.jsx
+++ src/components/PostLink/index.jsx
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 
 import { Link } from 'gatsby';
 
+import Category from '@components/Category';
+
 function PostLink(props) {
   return (
     <Link className="block mt-10" to={props.link}>
       <div className="p-5 -m-5 flex flex-col items-start hover:bg-blue-100 hover:bg-opacity-50 transition duration-300 ease-linear">
-        <span className="inline-block py-1 px-3 rounded bg-blue-100 text-blue-500 text-sm font-medium tracking-widest">{props.category}</span>
+        <Category text={props.category} active />
         <h2 className="sm:text-3xl text-2xl title-font font-black text-gray-900 mt-4 mb-4">{props.title}</h2>
         <p className="leading-relaxed">{props.contents}</p>
       </div>
