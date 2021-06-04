a0c6172c4f252d27686c6b1e57af8f9d76939406
minung.han <hmu332233@gmail.com>
2021-06-05 01:31:58 +0900

    feat: post background round 처리

--- src/components/PostLink/index.jsx
+++ src/components/PostLink/index.jsx
@@ -8,7 +8,7 @@ import Category from '@components/Category';
 function PostLink(props) {
   return (
     <Link className="block mt-10" to={props.link}>
-      <div className="p-5 -m-5 flex flex-col items-start hover:bg-blue-100 hover:bg-opacity-50 transition duration-300 ease-linear">
+      <div className="p-5 -m-5 flex flex-col items-start hover:bg-blue-100 hover:bg-opacity-50 transition duration-300 ease-linear rounded">
         <Category text={props.category} active />
         <h2 className="sm:text-3xl text-2xl title-font font-black text-gray-900 mt-4 mb-4">
           {props.title}
