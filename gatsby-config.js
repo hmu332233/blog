2f8377420a1e6b6ec1e3449fb82b0c2f5b059c51
minung.han <hmu332233@gmail.com>
2021-06-18 01:58:46 +0900

    refactor: posts 디렉토리 이동

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`,
-        path: `${__dirname}/posts`,
+        path: `${__dirname}/contents/posts`,
       },
     },
     {
