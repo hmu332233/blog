8a0e35bf772fd3cd9c57ce9012c496cdd89d292a
minung.han <hmu332233@gmail.com>
2021-07-14 01:58:23 +0900

    feat: 프로필이 최상단에 있을 경우 margin top이 없도록 수정

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 function Profile(props) {
   return (
-    <div className="flex mt-10 mb-8">
+    <div className="flex first:mt-0 mt-10 mb-8">
       <img className="h-16 w-16 rounded-full" src={props.image} />
       <div className="ml-4">
         <h2 className="font-medium">{props.name}</h2>
