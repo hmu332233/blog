9529869f80920b756b4411c5b7570753790e8d37
Minung Han <hmu332233@gmail.com>
2022-09-03 02:02:10 +0900

    feat: QuickSearch 블로그에 적용

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -4,6 +4,7 @@ import { useStaticQuery, graphql } from 'gatsby';
 
 import Header from '@components/Header';
 import Footer from '@components/Footer';
+import QuickSearch from '@components/QuickSearch';
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -19,20 +20,23 @@ const Layout = ({ children }) => {
   `);
 
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <main>
-        <section className="text-gray-700 body-font">
-          <div className="container flex flex-col px-5 py-12 md:py-24 mx-auto max-w-screen-sm">
-            {children}
-          </div>
-        </section>
-      </main>
-      <Footer
-        author={data.site.siteMetadata.author}
-        githubLink={data.site.siteMetadata.githubLink}
-      />
-    </div>
+    <>
+      <div className="flex flex-col min-h-screen">
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <main>
+          <section className="text-gray-700 body-font">
+            <div className="container flex flex-col px-5 py-12 md:py-24 mx-auto max-w-screen-sm">
+              {children}
+            </div>
+          </section>
+        </main>
+        <Footer
+          author={data.site.siteMetadata.author}
+          githubLink={data.site.siteMetadata.githubLink}
+        />
+      </div>
+      <QuickSearch />
+    </>
   );
 };
 
